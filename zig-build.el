294af8ae024f18ace11be8a06365f01c5189d247
Haskellknippe <kristian_hasselknippe@outlook.com>
2019-07-06 00:40:26 +0200

    finished transient command for zig build

--- zig-build.el
+++ zig-build.el
@@ -1,5 +1,12 @@
 (require 'transient)
 
+(defmacro define-zig-build-infix (name description short arg)
+  `(define-infix-argument ,(intern (format "zig-build:%s" name)) ()
+	 :description ,description
+	 :class 'transient-option
+	 :shortarg ,short
+	 :argument ,(format "%s " arg)))
+
 (defun run-zig-build-command (&rest args)
   (interactive (transient-args))
   (message (format "zig build %s" (string-join args " "))))
@@ -17,51 +24,34 @@
   :shortarg "-m"
   :argument "--message="))
 
+(define-zig-build-infix "--prefix" "Override default install prefix, libraries, headers" "-p" "--prefix")
+(define-zig-build-infix "--search-prefix" "Add a path to look for binaries, libraries, headers" "-P" "--search-prefix")
+
+(define-zig-build-infix "--build-file" "Override path to build.zig" "-B" "--build-file")
+(define-zig-build-infix "--cache-dir" "Override path to zig cache directory" "-B" "--cache-dir")
+(define-zig-build-infix "--override-std-dir" "Override path to Zig standard library" "-o" "--override-std-dir")
+(define-zig-build-infix "--override-lib-dir" "Override path to Zig lib directory" "-O" "--override-lib-dir")
+
 (define-transient-command zig-build () "Zig build"
-  ["Arguments"
+  ["General options"
    ("-v" "Print commands before executing them" "--verbose")
    (zig-build:--prefix)
    (zig-build:--search-prefix)]
+  ["Project-Specifig Options"
+   ("=s" "optimizatinos on and safety on" "-Drelease-safe=true")
+   ("=f" "optimizations on and safety off" "-Drelease-fast=true")
+   ("=S" "size optimizations on and safety off" "-Drelease-small=true")]
+  ["Advanced options"
+   (zig-build:--build-file)
+   (zig-build:--cache-dir)
+   (zig-build:--override-std-dir)
+   (zig-build:--override-lib-dir)
+   ("@t" "Enable compiler debug output for tokenization" "--verbose-tokenize")
+   ("@a" "Enable compiler debug output for parsing into an AST" "--verbose-ast")
+   ("@l" "Enable compiler debug output for linking" "--verbose-link")
+   ("@i" "Enable compiler debug output for Zig IR" "--verbose-ir")
+   ("@L" "Enable compiler debug output for LLVM IR" "--verbose-llvm-ir")
+   ("@c" "Enable compiler debug output for C imports" "--verbose-cimport")
+   ("@C" "Enable compiler debug output for C compilation" "--verbose-cc")]
   ["Actions"
    ("b" "Build" run-zig-build-command)])
-
-(define-infix-argument zig-build:--prefix ()
-  :description "Override default install prefix"
-  :class 'transient-option
-  :shortarg "-p"
-  :argument "--prefix ")
-
-(defmacro define-zig-build-infix (name description short arg)
-  `(define-infix-argument ,(intern (format "zig-build:%s" name)) ()
-	 :description ,description
-	 :class 'transient-option
-	 :shortarg ,short
-	 :argument ,(format "%s " arg)))
-
-(define-zig-build-infix "--search-prefix" "Add a path to look for binaries, libraries, headers" "-P" "--search-prefix")
-
-;;General Options:
-;;  --help                 Print this help and exit
-;;  --verbose              Print commands before executing them
-;;  --prefix [path]        Override default install prefix
-;;  --search-prefix [path] Add a path to look for binaries, libraries, headers
-;;
-;;Project-Specific Options:
-;;  -Drelease-safe=[bool]  optimizations on and safety on
-;;  -Drelease-fast=[bool]  optimizations on and safety off
-;;  -Drelease-small=[bool] size optimizations on and safety off
-;;
-;;Advanced Options:
-;;  --build-file [file]      Override path to build.zig
-;;  --cache-dir [path]       Override path to zig cache directory
-;;  --override-std-dir [arg] Override path to Zig standard library
-;;  --override-lib-dir [arg] Override path to Zig lib directory
-;;  --verbose-tokenize       Enable compiler debug output for tokenization
-;;  --verbose-ast            Enable compiler debug output for parsing into an AST
-;;  --verbose-link           Enable compiler debug output for linking
-;;  --verbose-ir             Enable compiler debug output for Zig IR
-;;  --verbose-llvm-ir        Enable compiler debug output for LLVM IR
-;;  --verbose-cimport        Enable compiler debug output for C imports
-;;  --verbose-cc             Enable compiler debug output for C compilation
-
-
